ae822c566039abaadb1f6e623125996931e8cbdb
toluolatunde <45163205+toluolatunde@users.noreply.github.com>
2020-09-18 11:25:25 +0100

    first commit

---
+++ src/com/thefirst/FirstClass.java
@@ -0,0 +1,25 @@
+package com.thefirst;
+
+import java.util.Date;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//byte myAge = 45;
+		//int viewsCount = 123_332_433;
+		//float price = 10.99F;
+		//char letter = 'A';
+		//boolean likedVideo = true;
+		
+		Date now = new Date();
+		System.out.println(now);
+		
+		
+		//System.out.println("Hello World");
+		//System.out.println(herAge);
+		
+	}
+
+}
---
+++ src/com/thefirst/movingForward.java
@@ -0,0 +1,31 @@
+ package com.thefirst;
+
+import java.awt.*;
+
+public class movingForward {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Point point1 = new Point(1, 1);
+//		Point point2 = point1;
+//		point1.x = 2;
+		
+		String message = "You are welcome" + " Tolu Olatunde";
+		
+		
+		
+		System.out.println(message.replace("Tolu", "Bolu"));
+		
+		//This is used to pick a particular value in variable
+	//	System.out.println(message.indexOf("w"));
+		
+		//This shows the lenght of the variable passed
+		//System.out.println(message.length());
+		
+		//This will respond with boolean true or false of 
+		//System.out.println(message.startsWith("are"));
+		
+	}
+
+}
